be97a476a0a25ccc6009a1f8d1f160f4f11e9e09
Daniel Schaaff <daniel.schaaff@verve.com>
2018-05-16 16:20:48 -0700

    got list clusters command working

--- lib/ecs_cmd/clusters.rb
+++ lib/ecs_cmd/clusters.rb
@@ -14,14 +14,23 @@ module EcsCmd
     def get_container_instance_count(cluster)
       @client.list_container_instances(cluster: cluster)[0].length
     end
-
-    def get_service_count
+    # TODO: handle paginated responses for when there is more than 100 services
+    def get_service_count(cluster)
+      count = @client.list_services(cluster: cluster, max_results: 100)[0].length
     end
 
-    def get_running_task_count
+    def get_tasks(cluster)
+      @client.list_tasks(cluster: cluster)[0]
     end
 
-    def get_pending_task_count
+    def get_running_task_count(cluster)
+      stats = @client.describe_clusters({clusters: [cluster]})[0][0]
+      stats['running_tasks_count']
+    end
+
+    def get_pending_task_count(cluster)
+      stats = @client.describe_clusters({clusters: [cluster]})[0][0]
+      stats['pending_tasks_count']
     end
 
     def list_clusters
@@ -29,9 +38,9 @@ module EcsCmd
       rows = []
       @clusters.map do |c|
         cluster_name = c.split('/')[1] 
-        rows << [cluster_name, get_container_instance_count(cluster_name)] 
+        rows << [cluster_name, get_container_instance_count(cluster_name), get_service_count(cluster_name), get_running_task_count(cluster_name), get_pending_task_count(cluster_name)] 
       end
-      table = Terminal::Table.new :headings => ['CLUSTER NAME', 'CONTAINER_INSTANCE_COUNT'], :rows => rows
+      table = Terminal::Table.new :headings => ['CLUSTER NAME', 'CONTAINER_INSTANCE_COUNT', 'SERVICES', 'RUNNING_TASKS', 'PENDING_TASKS'], :rows => rows
       puts table
     end
   end
