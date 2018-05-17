f14d8452bbfbfe5e371193efee8ffade786626a6
Daniel Schaaff <daniel.schaaff@verve.com>
2018-05-17 10:45:23 -0700

    small optimization to clusters methods

--- lib/ecs_cmd/clusters.rb
+++ lib/ecs_cmd/clusters.rb
@@ -10,26 +10,24 @@ module EcsCmd
     def get_clusters
       @cluster_arns = @client.list_clusters 
     end
-
-    def get_container_instance_count(cluster)
-      @client.list_container_instances(cluster: cluster)[0].length
-    end
-    # TODO: handle paginated responses for when there is more than 100 services
-    def get_service_count(cluster)
-      count = @client.list_services(cluster: cluster, max_results: 100)[0].length
+    
+    def get_container_instance_count(stats)
+      # stats = @client.describe_clusters(clusters: [cluster])[0][0]
+      stats['registered_container_instances_count']
     end
 
-    def get_tasks(cluster)
-      @client.list_tasks(cluster: cluster)[0]
+    def get_service_count(stats)
+      # stats = @client.describe_clusters(clusters: [cluster])[0][0]
+      stats['active_services_count']
     end
 
-    def get_running_task_count(cluster)
-      stats = @client.describe_clusters({clusters: [cluster]})[0][0]
+    def get_running_task_count(stats)
+      # stats = @client.describe_clusters({clusters: [cluster]})[0][0]
       stats['running_tasks_count']
     end
 
-    def get_pending_task_count(cluster)
-      stats = @client.describe_clusters({clusters: [cluster]})[0][0]
+    def get_pending_task_count(stats)
+      # stats = @client.describe_clusters({clusters: [cluster]})[0][0]
       stats['pending_tasks_count']
     end
 
@@ -37,8 +35,9 @@ module EcsCmd
       @clusters = get_clusters[0]
       rows = []
       @clusters.map do |c|
-        cluster_name = c.split('/')[1] 
-        rows << [cluster_name, get_container_instance_count(cluster_name), get_service_count(cluster_name), get_running_task_count(cluster_name), get_pending_task_count(cluster_name)] 
+        cluster_name = c.split('/')[1]
+        stats = @client.describe_clusters(clusters: [cluster_name])[0][0] 
+        rows << [cluster_name, get_container_instance_count(stats), get_service_count(stats), get_running_task_count(stats), get_pending_task_count(stats)] 
       end
       table = Terminal::Table.new :headings => ['CLUSTER NAME', 'CONTAINER_INSTANCE_COUNT', 'SERVICES', 'RUNNING_TASKS', 'PENDING_TASKS'], :rows => rows
       puts table
