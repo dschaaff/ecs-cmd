6e13cd1a8d4b71a3520cd14989437795e4313094
Daniel Schaaff <daniel.schaaff@verve.com>
2018-07-27 09:08:17 -0700

    update to use proper gli subcommands

--- lib/ecs_cmd/services.rb
+++ lib/ecs_cmd/services.rb
@@ -5,6 +5,7 @@ module EcsCmd
   class Services
     def initialize(region)
       @client = Aws::ECS::Client.new(region: region)
+      @reg = region
     end
 
     def get_services(cluster)
@@ -16,12 +17,12 @@ module EcsCmd
       @service_arns
     end
 
-    def list_services(cluster)
+    def list_services(cluster, reg = @reg)
       @service_list = get_services(cluster)
       rows = []
       @service_list.map do |service|
         service_name = service.split('/')[1]
-        serv = EcsCmd::Service.new(cluster, service_name)
+        serv = EcsCmd::Service.new(cluster, service_name, reg)
         rows << [service_name, serv.desired_count, serv.running_count, serv.pending_count]
       end
       table = Terminal::Table.new headings: ['SERVICE NAME', 'DESIRED_COUNT',
