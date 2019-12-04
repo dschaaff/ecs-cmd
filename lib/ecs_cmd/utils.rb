5f969b678ab7ba513ae436812c9a86fb8fad0353
Daniel Schaaff <dschaaff@cordial.com>
2019-12-03 17:52:01 -0800

    fix for using docker hub images

--- lib/ecs_cmd/utils.rb
+++ lib/ecs_cmd/utils.rb
@@ -1,9 +1,11 @@
 module EcsCmd
   module Utils
     def self.parse_image_name(image)
-      regex = /^([a-zA-Z0-9\.\-]+):?([0-9]+)?\/([a-zA-Z0-9\._\-]+)(\/[\/a-zA-Z0-9\._\-]+)?:?([a-zA-Z0-9\._\-]+)?$/
+      regex = /^([a-zA-Z0-9\.\-]+):?([0-9]+)?\/?([a-zA-Z0-9\._\-]+)(\/[\/a-zA-Z0-9\._\-]+)?:?([a-zA-Z0-9\._\-]+)?$/
       raise 'invalid image supplied, please verify correct image format' unless regex.match(image)
-      if regex.match(image)[4].nil? || regex.match(image)[4] == false
+      if regex.match(image)[5].nil? || regex.match(image)[5] == false
+        regex.match(image)[1]
+      elsif regex.match(image)[4].nil? || regex.match(image)[4] == false
         regex.match(image)[3]
       else
         regex.match(image)[4].gsub(/\//, '')
